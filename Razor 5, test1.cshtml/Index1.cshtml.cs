e012561ff52382cdf61175df0a866406b933de3d
Yiğit Oğuz <ogsano79@gmail.com>
2026-04-03 12:05:24 +0300

    Add files via upload

---
+++ Razor 5, test1.cshtml/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razor_4_SINIF_İÇİ_ETKİNLİK.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
