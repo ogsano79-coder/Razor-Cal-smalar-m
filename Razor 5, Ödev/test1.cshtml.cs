d7074671749a0f1bf97ab968dcdd1609c257251f
Yiğit Oğuz <ogsano79@gmail.com>
2026-04-11 13:42:42 +0300

    Add files via upload

---
+++ Razor 5, Ödev/test1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication4.Pages
+{
+    public class test1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
