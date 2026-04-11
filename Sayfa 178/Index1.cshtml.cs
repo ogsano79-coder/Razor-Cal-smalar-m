1d9fac2c77792898fcbb22994b547d07cfbc859e
Yiğit Oğuz <ogsano79@gmail.com>
2026-04-11 16:49:10 +0300

    Add files via upload

---
+++ Sayfa 178/Index1.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KareAlApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
